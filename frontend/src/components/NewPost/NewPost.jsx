7c98e1a97bc75f3221c62dd812cbf4bc5f00a209
NguyenVietTrung <117735066+viettrung2512@users.noreply.github.com>
2025-05-26 13:47:01 +0700

    .

--- frontend/src/components/NewPost/NewPost.jsx
+++ frontend/src/components/NewPost/NewPost.jsx
@@ -101,7 +101,7 @@ const NewPost = ({ token }) => {
     formData.append("image", imageFile);
 
     try {
-      const response = await fetch("http://localhost:8080/cloudinary/upload", {
+      const response = await fetch("https://social-web-axbp.onrender.com/cloudinary/upload", {
         method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
